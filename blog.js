a39c87cce12ee2dcb7d48e1de8fe07ecee9cf4f8
shubhangi.goel <shubhangi.goel@cricbuzz.com>
2017-01-08 15:39:47 +0530

    added comments, resizing issue

---
+++ blog.js
@@ -0,0 +1,52 @@
+var blogIndexApp = angular.module('blogIndexApp', ['ngRoute']);
+blogIndexApp.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+      when('/main', {
+		templateUrl: 'main.html',
+		controller: 'blogIndexController'
+	}).
+      when('/blogDetail/:blogId', {
+		templateUrl: 'blog_detail.html',
+		controller: 'blogIndexController'
+      }).
+      otherwise({
+		redirectTo: '/main'
+      });
+}]);
+blogIndexApp.controller('blogIndexController',["$scope","$http", "$window", "blogService",function($scope, $http, $window, blogService){
+  $scope.blogList = {}
+  $scope.authorName = []
+  $scope.profileColorArr  = ["#4ECDC4", "#FF6B6B", "#FE4365", "#033649", "#83AF9B",
+   "#ECD078", "#C02942", "#53777A", "#556270", "#C7F464",
+  "#490A3D", "#BD1550", "#E6AC27"]
+  $scope.getBlogList = function(){
+    $http.get("http://35.154.87.133:9000/?page=0&&size=10").success(function(response){
+      console.log(response)
+      $scope.authorList = response.authors
+      $scope.blogList = response.blogs
+    }).error(function(e){
+      console.log(e)
+    });
+  }
+  $scope.setAuthor = function(authorIds){
+    //  var authorIds = Array.prototype.slice.call(arguments);
+      for(var id in authorIds)
+        for(var author in $scope.authorList)
+          if(authorIds[id]==$scope.authorList[author].id)
+              $scope.authorName.push($scope.authorList[author].name)
+  }
+  $scope.readMoreBlog = function(blogId){
+    $http.get("http://35.154.87.133:9000/blog/"+blogId).success(function(response){
+      console.log(response)
+      $scope.blogDetail = response
+    //  $window.location.href = "file:///home/shubhangi.goel/myprojects/frontend/blog_detail.html"
+    }).error(function(e){
+      console.log(e)
+    });
+  }
+
+$scope.addComment = function(){
+
+}
+}]);
