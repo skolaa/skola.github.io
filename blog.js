6dfbc9accb2c1a45db6f9f767b92556036c4f7b5
shubhangi.goel <shubhangi.goel@cricbuzz.com>
2017-05-07 20:11:38 +0530

    added pagination, facebook comments

--- blog.js
+++ blog.js
@@ -1,6 +1,6 @@
 var blogIndexApp = angular.module('blogIndexApp', ['ngRoute', 'ngSanitize']);
-blogIndexApp.config(['$routeProvider','$httpProvider',
-  function($routeProvider,$httpProvider) {
+blogIndexApp.config(['$routeProvider','$httpProvider', '$locationProvider',
+  function($routeProvider,$httpProvider, $locationProvider) {
     $httpProvider.defaults.withCredentials = true;
     $routeProvider.
       when('/main', {
@@ -15,34 +15,73 @@ blogIndexApp.config(['$routeProvider','$httpProvider',
 		redirectTo: '/main'
       });
 }]);
-blogIndexApp.controller('blogIndexController',["$scope","$http", "$window","$routeParams", "$sce",function($scope, $http, $window, $routeParams, $sce){
+
+blogIndexApp.controller('blogIndexController',["$scope","$http", "$window","$routeParams", "$sce","appConfig", "selectedBlog", "$location",function($scope, $http, $window, $routeParams, $sce, appConfig, selectedBlog, $location){
   $scope.blogList = {}
-  $scope.profileColorArr  = ["#4ECDC4", "#FF6B6B", "#FE4365", "#033649", "#83AF9B",
-   "#ECD078", "#C02942", "#53777A", "#556270", "#C7F464",
-  "#490A3D", "#BD1550", "#E6AC27"]
-  $scope.getBlogList = function(){
-    $http.get("http://35.154.87.133:9000/?page=0&&size=10").success(function(response){
-      $scope.authorList = response.authors
-      $scope.blogList = response.blogs
-    }).error(function(e){
-      console.log(e)
-    });
+  $scope.selectedPage = 0;
+
+  $scope.getUrl = function(title) {
+        return title.replace(/\s/g, '-').toLowerCase();
   }
+  $scope.getBlogList = function(pageIndex) {
+        $http.get(appConfig.STATIC_URL + "?page=" + pageIndex + "&&size=10").success(function(response) {
+            $scope.authorList = response.authors
+            $scope.blogList = response.blogs
+            $scope.blogList.content.forEach(function(blog) {
+                blog.url = $scope.getUrl(blog.blogTitle);
+            });
+            $scope.blogList.totalPages = 10
+            $scope.pagesArr = Array.from(Array($scope.blogList.totalPages), () => 0);
+            // for (var i = 0; i < $scope.blogList.totalPages; i++) {
+            //     $scope.pagesArr.push(0);
+            // }
+        }).error(function(e) {
+            console.log(e)
+        });
+    }
   $scope.setAuthor = function(authorIds){
     var names = $scope.authorList.filter(author => authorIds.indexOf(author.id) != -1).map(author => author.name);
     return names;
   }
-  $scope.readMoreBlog = function(){
-    var blogId = $routeParams.blogId;
-    $http.get("http://35.154.87.133:9000/blog/"+blogId).success(function(response){
-      $scope.blogDetail = response
-      $scope.blogDetail.blogText = marked($scope.blogDetail.blogText);
-    }).error(function(e){
-      console.log(e)
-    });
+
+  $scope.subscribe = function(email) {
+	var params = { 'email': email }
+	$http.post(appConfig.STATIC_URL + "subscribe", params).success(function(response) {
+	    console.log("done");
+	}).error(function(response) {
+	    console.log("error");
+	})
   }
-}
-]);
+
+    $scope.getPrev = function() {
+        $scope.selectedPage -= 1
+        $scope.getBlogList($scope.selectedPage);
+    }
+    $scope.getNext = function() {
+        $scope.selectedPage += 1
+        $scope.getBlogList($scope.selectedPage);
+    }
+    $scope.getSelectedPage = function(index) {
+        $scope.selectedPage = index
+        $scope.getBlogList($scope.selectedPage)
+    }
+    $scope.showHideSubscribe = function() {
+        var element = document.getElementById('subscribeDiv');
+        element.classList.toggle('closed');
+    }
+
+    $scope.readMoreBlog = function() {
+        var id = $routeParams.blogId
+        $http.get(appConfig.STATIC_URL + "blog/" + id).success(function(response) {
+            $scope.blogDetail = response
+            $scope.tags = $scope.blogDetail.tags
+            $scope.relatedStories = $scope.blogDetail.relatedStories
+            $scope.blogDetail.blogText = marked($scope.blogDetail.blogText);
+        }).error(function(e) {
+            console.log(e)
+        });
+    }
+}]);
 
 marked.setOptions({
     renderer: new marked.Renderer(),
@@ -53,11 +92,30 @@ marked.setOptions({
     sanitize: false, // if false -> allow plain old HTML ;)
     smartLists: true,
     smartypants: false,
-    highlight: function (code, lang) {
-      if (lang) {
-        return hljs.highlight(lang, code).value;
-      } else {
-        return hljs.highlightAuto(code).value;
-      }
+    highlight: function(code, lang) {
+        if (lang) {
+            return hljs.highlight(lang, code).value;
+        } else {
+            return hljs.highlightAuto(code).value;
+        }
     }
-  });
+});
+
+blogIndexApp.constant('appConfig', {
+    'STATIC_URL': 'http://35.154.87.133:9000/'
+});
+blogIndexApp.factory("selectedBlog", function() {
+    var currentBlog = {}
+    return {
+        set: setData,
+        get: getData
+    }
+
+    function setData(data) {
+        currentBlog = data
+    };
+
+    function getData() {
+        return currentBlog;
+    };
+});
